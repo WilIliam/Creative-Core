f05fe43f855e227d1650a61fea34332e6135bdde
WilIliam <long.le@indigames.net>
2022-06-02 11:09:09 +0700

    Done Creative Core!!!

---
+++ CC_Prototyping/Assets/CreativeCore_Prototyping/StarterAssets/Editor/FirstPersonStarterAssetsDeployMenu.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using UnityEditor;
+using UnityEngine;
+
+namespace StarterAssets
+{
+    public partial class StarterAssetsDeployMenu : ScriptableObject
+    {
+        // prefab paths
+        private const string FirstPersonPrefabPath = "/FirstPersonController/Prefabs/";
+
+#if STARTER_ASSETS_PACKAGES_CHECKED
+        /// <summary>
+        /// Check the capsule, main camera, cinemachine virtual camera, camera target and references
+        /// </summary>
+        [MenuItem(MenuRoot + "/Reset First Person Controller", false)]
+        static void ResetFirstPersonControllerCapsule()
+        {
+            var firstPersonControllers = FindObjectsOfType<FirstPersonController>();
+            var player = firstPersonControllers.FirstOrDefault(controller => controller.CompareTag(PlayerTag));
+            GameObject playerGameObject;
+
+            // player
+            if (player == null)
+            {
+
+                // HandleInstantiatingPrefab(StarterAssetsPath + FirstPersonPrefabPath,
+                // PlayerCapsulePrefabName, out playerGameObject);
+                Debug.Log("Couldn't find player prefab");
+                playerGameObject = player.gameObject;
+            }
+            else
+                playerGameObject = player.gameObject;
+
+
+            // cameras
+            CheckCameras(playerGameObject.transform, FirstPersonPrefabPath);
+        }
+#endif
+    }
+}
